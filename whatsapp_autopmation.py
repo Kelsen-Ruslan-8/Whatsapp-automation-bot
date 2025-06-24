31a7df6046e70742def4733b1630203255ea5a7c
kelsen <kelsenruslan888@gmai.com>
2025-06-24 15:27:40 +0700

    add whatsapp_automation file

---
+++ whatsapp_autopmation.py
@@ -0,0 +1,15 @@
+name = input('Name:')
+phone_num = input('Phone_number:')
+Message = input('What_message_to_send:')
+hours_time = input('What_hours_to_send:')
+minutes_time = input('What_minutes_to_send:')
+
+
+
+print('Saving', ',' , name, ',' , phone_num, '"', Message,'"', ',', hours_time,',', minutes_time  )
+
+import csv 
+
+with open('contact.csv', mode='a',newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([name, phone_num, Message, hours_time, minutes_time])
