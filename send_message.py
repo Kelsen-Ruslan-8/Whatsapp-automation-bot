3335d410560d2937857de56f579d5d980d914f48
kelsen <kelsenruslan888@gmai.com>
2025-06-27 01:15:25 +0700

    Add whatsapp-automataion file

--- send_message.py
+++ send_message.py
@@ -1,31 +1,9 @@
-import csv
 import pywhatkit
-import pyautogui
-import time
+
+def send_message(phone, message, hour, minute):
+    pywhatkit.sendwhatmsg(phone, message, hour, minute, wait_time=20)
 
 
-
-with open('contacts.csv', mode='r') as file:
-    reader = csv.reader(file)
-    #next(reader)
-
-    for row in reader:
-        name = row[0]
-        phone = row[1]
-        message = row[2]
-        hour = int(row[3])
-        minute = int(row[4])
-
-        print(f'Sending message to {name} at {hour}:{minute}...')
-        try:
-            pywhatkit.sendwhatmsg(phone, message, hour, minute)
-            time.sleep(15)
-            pyautogui.click()
-            pyautogui.press('enter')
-            print(f'message to {name}scheduled.\n')
-        except Exception as e:
-            print(f'failed to send message to {name}. Error: {e}')
-        time.sleep(15)
 
 
 
